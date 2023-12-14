ba25037e11bc45710ca9706c7311b3561228abaf
gustavnogood <105938111+gustavnogood@users.noreply.github.com>
2023-12-14 22:13:40 +0100

    lagt till prisma och postgres
    har fått den att prata med databasen

--- server/index.js
+++ server/index.js
@@ -1,27 +1,34 @@
-import express from "express";
-import path from "path";
-import homepageRouter from "./homepageRouter.js";
-import assetsRouter from "./assetsRouter.js";
+import express from 'express';
+import path from 'path';
+import homepageRouter from './homepageRouter.js';
+import assetsRouter from './assetsRouter.js';
+import { runPrismaTest } from './prismaTest.js'; // Uppdatera sökvägen beroende på din struktur
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join(path.resolve(), "public");
-const distPath = path.join(path.resolve(), "dist");
+const publicPath = path.join(path.resolve(), 'public');
+const distPath = path.join(path.resolve(), 'dist');
 
 const app = express();
 
-app.get("/api/v1/hello", (_req, res) => {
-  res.json({ message: "Hello, world!" });
+
+
+app.get('/api/v1/hello', (_req, res) => {
+  res.json({ message: 'Hello, world!' });
 });
 
-if (process.env.NODE_ENV === "production") {
-      app.use("/", express.static(distPath));
-    } else {
-      app.use("/", express.static(publicPath));
-      app.use("/src", assetsRouter);
-    }
-    
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(distPath));
+} else {
+  app.use('/', express.static(publicPath));
+  app.use('/src', assetsRouter);
+}
+
 app.use(homepageRouter);
 
+runPrismaTest();
+
 app.listen(port, () => {
-  console.log("Server listening on port", port);
+  console.log('Server listening on port', port);
 });
+
+
